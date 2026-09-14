85c6a47e7e1b66c7b1dddea51ab07c2272a83566
igchek <igchek2014@gmail.com>
2026-09-15 02:36:14 +0300

    base up and running

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,15 @@
 import {PrismaClient} from "@prisma/client";
 
+declare global {
+    interface BigInt {
+        toJSON():number
+    }
+}
+
+BigInt.prototype.toJSON = function () {
+    return Number(this);
+}
+
 const globalForPrisma = globalThis as unknown as {prisma:PrismaClient|undefined}
 
 export const prisma = globalForPrisma.prisma ?? new PrismaClient({
